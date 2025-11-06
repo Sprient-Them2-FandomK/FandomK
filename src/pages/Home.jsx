1cf10f1e0a8622f1124c3e42952fce38720b46ab
wjdthdms <soeun1327@gmail.com>
2025-11-06 20:39:26 +0900

    첫 번째 section 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./Home.module.css";
+import imgHome1 from "../assets/imgs/home1.png";
+import imgLogo from "../assets/imgs/logo.svg";
+
 function Home() {
+
+  const navigate = useNavigate();
+
+  const goListPage = () => {
+    localStorage.clear();
+    navigate('/list');
+  }
+
   return (
-    <>
-      <div>나는 홈페이지임</div>
-    </>
+    <div className={styles.container}>
+
+      <section className={styles.firstSection}>
+        <div className={styles.firstSectionBox}>
+
+          <img src={imgHome1}/>
+
+          <div className={styles.logoContainer}>
+            <div>내가 좋아하는 아이돌을<br/>가장 <span>쉽게 덕질</span> 하는 방법</div>
+            <img src={imgLogo}/>
+          </div>
+
+          <button onClick={goListPage} className={styles.startButton}>지금 시작하기</button>
+
+        </div>
+      </section>
+        
+      {/* <section>
+        <div className={styles.sectionBox}>
+
+          <img src="../assets/imgs/home2.png"/>
+
+          <div className={styles.logoContainer}>
+            <div>후원하기</div>
+            <div>좋아하는 아이돌에게<br/>쉽게 조공해 보세요</div>
+          </div>
+
+        </div>
+      </section> */}
+
+      <section>
+        
+      </section>
+
+      <section>
+        
+      </section>
+
+    </div>
   );
 }
 
