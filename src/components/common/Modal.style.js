aee9b1efaeb86ead6a3b043fe48ba9070be92cfa
spde3289 <spde3289@naver.com>
2025-11-15 15:50:42 +0900

    fix: 모달창 css 포지션 수정 absolute -> fixed

--- src/components/common/Modal.style.js
+++ src/components/common/Modal.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  position: absolute;
+  position: fixed;
   inset: 0;
   background-color: #000000cc;
   display: flex;
